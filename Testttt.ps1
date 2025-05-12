8e6ba2eadf2672f1e4cbabbf0b30ab8e4fd47692
Benjamin Rain <tulak@hord.dev>
2025-05-12 10:45:23 +0300

    Removed some stuff

--- Testttt.ps1
+++
@@ -1,5 +0,0 @@
-$userUPN = "Itay.Rosenfeld1@TCSM.onmicrosoft.com"  # Replace with actual UPN
-Get-MgUser -UserId $userUPN | Select-Object ProxyAddresses
-
-$userUPN2 = "itay.rosenfeld@techsomed.com"  # Replace with actual UPN
-Get-MgUser -UserId $userUPN2 | Select-Object ProxyAddresses
